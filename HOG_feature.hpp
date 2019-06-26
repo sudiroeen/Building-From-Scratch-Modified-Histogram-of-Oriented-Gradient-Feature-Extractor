533a668e6695a9060da00efb80b2c68f236ea0c6
sudiroeen <44321165+sudiroeen@users.noreply.github.com>
2019-06-26 14:45:54 +0700

    Add files via upload

---
+++ HOG_feature.hpp
@@ -0,0 +1,30 @@
+/*
+ Author:
+
+ 	Sudiro 
+ 	   [at] SudiroEEN@gmail.com
+*/
+
+#ifndef HOG_FEATURE_HPP
+#define HOG_FEATURE_HPP
+
+#include<opencv2/opencv.hpp>
+#include<iostream>
+
+using namespace std;
+using namespace cv;
+
+// #define DEBUG
+
+class HOG_feature{
+private:
+   string nameToSaveYAML;
+   Mat HOG_featureOfImage;
+   Mat img;
+
+public:
+   HOG_feature(Mat _img, string _nameToSaveYAML);
+   void getHOG_feature();
+   void saveFeature();
+};
+#endif
